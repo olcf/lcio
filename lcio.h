8979b58358d58622646e19cb08b7029ef13b63f3
Matthew Bachstein <mbachste@utk.edu>
2018-05-24 16:50:44 -0400

    created branch for metadata tests

--- lcio.h
+++ lcio.h
@@ -5,6 +5,29 @@
 #ifndef LCIO_LCIO_H
 #define LCIO_LCIO_H
 
+double get_time(void);
+double elapsed_time(double, double);
 
+/*
+ * lcio_engine_t describes the operations that lcio will perform.
+ * These are function pointers that can be initialized with any
+ * of the supported IO libraries (POSIX, MPI_IO, AIO) with the
+ * goal of creating a uniform interface for the function calls.
+ */
+typedef struct lcio_engine {
+    char *name;
+    void*(*create)(void*);
+    void*(*open)(void*);
+    void*(*close)(void*);
+    void*(*delete)(void*);
+    void*(*stat)(void*);
+    void*(*mkdir)(void*);
+    void*(*rmdir)(void*);
+    void*(*fsync)(void*);
+
+} lcio_engine_t;
+
+
+typedef struct
 
 #endif //LCIO_LCIO_H
--- time.c
+++ time.c
@@ -10,3 +10,18 @@
 /*
  * Timing structs for general use
  */
+
+double get_time(void){
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    return (double)(tv.tv_sec * 1000000) + (double)(tv.tv_usec);
+}
+
+double elapsed_time(double t1, double t2){
+    if (t1 > t2) return t1-t2;
+    else return t2-t1;
+}
+
+
+
