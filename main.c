a0dce734653f83cb4e22ed76719ba667cbfe99cf
Matthew Bachstein <mbachste@utk.edu>
2018-06-01 15:51:56 -0400

    tested MPI datatype send/recv. seems to work fine.

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ int main(int argc, char** argv) {
     int my_rank;
     int comm_sz;
     MPI_Comm group_comm;
+    lcio_job_t* myjob = malloc(sizeof(lcio_job_t));
 
     int num_jobs;
 
@@ -24,7 +25,7 @@ int main(int argc, char** argv) {
      * variables in lcio_job_t. These two are process specific,
      * and do not need to be broadcast.
      */
-    MPI_Datatype lcio_job_mpi;
+    MPI_Datatype MPI_LCIO_JOB;
     const int count = 9;
     int blens[count] = {32,32,16,8,1,1,1,1,1};
     MPI_Datatype array_of_types[count] =
@@ -38,7 +39,7 @@ int main(int argc, char** argv) {
     disps[0] = (MPI_Aint) 0; //lib_name
     disps[1] = cextent * 32; //tmp_dir
     disps[2] = disps[1] + (cextent * 32); //type
-    disps[3] = disps[2]+ (cextent * 16); //engine
+    disps[3] = disps[2] + (cextent * 16); //engine
     disps[4] = disps[3] + (cextent * 8); //num_pes
     disps[5] = disps[4] + iextent; //num_files
     disps[6] = disps[5] + iextent; //blk_sz
@@ -52,7 +53,8 @@ int main(int argc, char** argv) {
     /*
      * Create datatype for lcio_job_t
      */
-    MPI_Type_create_struct(count, blens, disps, array_of_types, &lcio_job_mpi);
+    MPI_Type_create_struct(count, blens, disps, array_of_types, &MPI_LCIO_JOB);
+    MPI_Type_commit(&MPI_LCIO_JOB);
 
 
     /*
@@ -69,10 +71,28 @@ int main(int argc, char** argv) {
         cfg = parse_conf_file(name);
         params = fill_parameters(cfg);
         num_jobs = params->num_jobs;
+        myjob = params->jobs[0];
         //print_cfg(cfg);
     }
     MPI_Bcast(&num_jobs, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    
+    MPI_Bcast(myjob, 1, MPI_LCIO_JOB, 0, MPI_COMM_WORLD);
+
+    if(my_rank == 1){
+        printf("recved");
+        printf("==============\n");
+        printf(" lib_name:%s\n", myjob->lib_name);
+        printf(" tmp_dir:%s\n", myjob->tmp_dir);
+        printf(" type:%s\n", myjob->type);
+        printf(" engine:%s\n", myjob->engine);
+        printf(" pes:%d\n", myjob->num_pes);
+        printf(" num_files:%d\n", myjob->num_files);
+        printf(" blk_sz:%lld\n", myjob->blk_sz);
+        printf(" fsync:%d\n", myjob->fsync);
+        printf(" mode:%c\n",myjob->mode);
+        fflush(stdout);
+    }
+
+
     //file_test(params->jobs[0]);
 
     MPI_Finalize();
